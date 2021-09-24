a33a1b7ccad7a050a20cf9158f3345db747b2af1
bloombar <amos@cs.nyu.edu>
2021-09-23 22:00:49 -0400

    simple debug route

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-'use strict'
+"use strict"
 
-const express = require('express') // app server
+const express = require("express") // app server
 
 // load routes
-const slackRouter = require('./routes/slackRouter')
-const watsonRouter = require('./routes/watsonRouter')
+const slackRouter = require("./routes/slackRouter")
+const watsonRouter = require("./routes/watsonRouter")
 
 // IBM Watson integration
-const { WatsonAss } = require('./helpers/watson/watsonAss')
-const AssistantV2 = require('ibm-watson/assistant/v2')
-const { IamAuthenticator } = require('ibm-watson/auth')
+const { WatsonAss } = require("./helpers/watson/watsonAss")
+const AssistantV2 = require("ibm-watson/assistant/v2")
+const { IamAuthenticator } = require("ibm-watson/auth")
 
 // Google Sheets integration
-const fs = require('fs')
-const googleSheets = require('./helpers/google/google-sheet')
+const fs = require("fs")
+const googleSheets = require("./helpers/google/google-sheet")
 // usage:
 //const rows = await googleSheets.read(process.env.GOOGLE_SHEET_ID, 'Overall!A:Z');
 //await googleSheets.append(process.env.GOOGLE_SHEET_ID, 'Access!Y:Z', [ ["1", "2", "3"], ["4", "5", "6"] ]);
 //await googleSheets.update(process.env.GOOGLE_SHEET_ID, 'Access!Y7:AA', [ ["A", "B", "C"], ["D", "E", "F"] ]);
 //await googleSheets.create('Example spreadsheet');
 
-const botApp = (config) => {
-  console.log('-- APP STARTING UP --')
+const botApp = config => {
+  console.log("-- APP STARTING UP --")
 
   // instantiate web server
   const app = express()
@@ -31,15 +31,20 @@ const botApp = (config) => {
 
   // load Slack API routes
   app.use(
-    '/api/slack',
+    "/api/slack",
     slackRouter({ config: config.slack, assistant: app.assistant })
   ) // pass the slack config settings to the router in an object
 
   // load Watson API routes
   app.use(
-    '/api/watson',
+    "/api/watson",
     watsonRouter({ config: config.watson, assistant: app.assistant })
   )
+
+  // a little debug route to check server
+  app.get("/debug", (req, res) => {
+    res.send("Debug!")
+  })
 
   return app
 }
