68daf7c5fe9f33e3ae3bd745f19d1b99455c36ce
bloombar <amos@cs.nyu.edu>
2021-09-23 21:27:30 -0400

    updated slack creds

--- routes/slackRouter.js
+++ routes/slackRouter.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require("express")
 
 // connect to slack events api
-const { createEventAdapter } = require('@slack/events-api')
-const { SlackAss } = require('../helpers/slack/slackAss')
+const { createEventAdapter } = require("@slack/events-api")
+const { SlackAss } = require("../helpers/slack/slackAss")
 
 const slackRouter = ({ config, assistant }) => {
   // create an express router
@@ -12,21 +12,21 @@ const slackRouter = ({ config, assistant }) => {
   const slackAss = new SlackAss({ config, assistant })
 
   // scrape list of slack users
-  router.get('/scrape-users', slackAss.scrapeUsers)
+  router.get("/scrape-users", slackAss.scrapeUsers)
 
   // connect to slack events api
   const slackEvents = createEventAdapter(config.signingSecret)
 
   // use slack's web api listener to handle incoming events
-  router.use('/events', slackEvents.requestListener())
+  router.use("/events", slackEvents.requestListener())
 
   // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
   // slackEvents.on('message', e => console.log(e)); // maybe deprecated
-  slackEvents.on('app_mention', slackAss.respondToMessage) // messages directed to the bot in channels
-  slackEvents.on('message', slackAss.respondToMessage) // direct messages to the bot
-  slackEvents.on('message.im', slackAss.respondToMessage)
-  slackEvents.on('message.groups', slackAss.respondToMessage)
-  slackEvents.on('error', (error) => {
+  slackEvents.on("app_mention", slackAss.respondToMessage) // messages directed to the bot in channels
+  slackEvents.on("message", slackAss.respondToMessage) // direct messages to the bot
+  slackEvents.on("message.im", slackAss.respondToMessage)
+  slackEvents.on("message.groups", slackAss.respondToMessage)
+  slackEvents.on("error", error => {
     console.error(`Slack event listener error: ${error}`)
   })
 
